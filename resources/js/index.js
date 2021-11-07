e9ac65645ec403432b08614e97c554b34ec1937b
Julia <62606982+themeriff117@users.noreply.github.com>
2021-11-07 10:50:52 -0500

    Added background and character sprites

--- resources/js/index.js
+++ resources/js/index.js
@@ -5,16 +5,24 @@
 let gameScene = new Phaser.Scene('Game');
 
 //load assets
-gameScene.preload = funcation() {
+gameScene.preload = function() {
   //load images
-  this.load.image('background', 'assets/')
+  this.load.image('background', 'assets/generic-rpg-pack/rpg-pack/tiles/generic-rpg-Slice.png');
+  this.load.spritesheet('warrior', 'assets/Warrior/SpriteSheet/Warrior_Sheet-Effect.png');
+}
+
+gameScene.create = funcation() {
+  //this.add.image(100, 100, 'background');
+  this.add.sprite(0, 0, 'background');
+
+  bg.setOrigin(0, 0);
 }
 
 //set the configuration of  the game
 let config = {
   type: Phaser.AUTO,
-  width: 640,
-  height: 360,
+  width: 100,
+  height: 100,
   scene: gameScene
 };
 //create a new game, pass the configuration
