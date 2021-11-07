baff5a5f00e844bd36b004eb2e015085250740d3
Julia <62606982+themeriff117@users.noreply.github.com>
2021-11-07 11:22:27 -0500

    trying to figure out how to make a tile map

--- resources/js/index.js
+++ resources/js/index.js
@@ -7,15 +7,26 @@ let gameScene = new Phaser.Scene('Game');
 //load assets
 gameScene.preload = function() {
   //load images
-  this.load.image('background', 'assets/generic-rpg-pack/rpg-pack/tiles/generic-rpg-Slice.png');
-  this.load.spritesheet('warrior', 'assets/Warrior/SpriteSheet/Warrior_Sheet-Effect.png');
+  //this.load.image('background', 'assets/generic-rpg-pack/rpg-pack/tiles/generic-rpg-Slice.png');
+  this.load.spritesheet('warrior', '../final-project/assets/Warrior/SpriteSheet/Warrior_Sheet-Effect.png');
+  this.load.image("bkg-tiles",  '../final-project/assets/generic-rpg-pack/rpg-pack/tiles');
 }
 
-gameScene.create = funcation() {
+gameScene.create = function() {
   //this.add.image(100, 100, 'background');
   this.add.sprite(0, 0, 'background');
 
   bg.setOrigin(0, 0);
+  bg.setPosition(50, 50);
+  let gameWidth = this.sys.game.config.width;
+  let gameHeight = this.sys.game.config.height;
+  console.log(gameWidth, gameHeight);
+  console.log(bg);
+  console.log(this);
+}
+
+gameScene.update = function() {
+
 }
 
 //set the configuration of  the game
@@ -23,7 +34,15 @@ let config = {
   type: Phaser.AUTO,
   width: 100,
   height: 100,
-  scene: gameScene
+  scene: gameScene;
+
+  parent: "game-container",
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
+
 };
 //create a new game, pass the configuration
 let game = new Phaser.Game(config);
