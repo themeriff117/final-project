5547868292121d4826b2efd40fb2ac6f52550507
Julia <62606982+themeriff117@users.noreply.github.com>
2021-11-06 21:41:41 -0400

    Downloaded Assets

---
+++ resources/js/index.js
@@ -0,0 +1,22 @@
+//NOTES FROM 'COMPLETE COURSE - PHASER 101 IN 1 HOUR'
+//aka game.js
+
+//create a new scene
+let gameScene = new Phaser.Scene('Game');
+
+//load assets
+gameScene.preload = funcation() {
+  //load images
+  this.load.image('background', 'assets/')
+}
+
+//set the configuration of  the game
+let config = {
+  type: Phaser.AUTO,
+  width: 640,
+  height: 360,
+  scene: gameScene
+};
+
+//create a new game, pass the configuration
+let game = new Phaser.Game(config);
