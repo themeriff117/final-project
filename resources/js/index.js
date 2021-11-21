f7e7ab490385365700d7783c6ca6b68f08872847
Julia <62606982+themeriff117@users.noreply.github.com>
2021-11-21 17:43:51 -0500

    Worked on tutorial to try and understand code

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,65 +1,119 @@
-import Phaser from 'phaser';
+//BOOTSCENE
+var BootScene = new Phaser.Class({
+    Extends: Phaser.Scene,
+    initialize:
+    function BootScene ()
+    {
+        Phaser.Scene.call(this, { key: 'BootScene' });
+    },
 
-let gameScene = new Phaser.Scene('Game');
-
-gameScene.preload = function() {
-  this.load.spritesheet('warrior', '../final-project/assets/Warrior/SpriteSheet/Warrior_Sheet-Effect.png');
-  //player = this.physics.add.sprite(32, 32, "warrior");
-  this.load.image('bkg-tiles', '../final-project/assets/full-bkg.json');
-}
-
-gameScene.create = function() {
-  //this.add.image(100, 100, 'background');
-  this.add.sprite(0, 0, 'bkg-tiles');
-
-  bg.setOrigin(0, 0);
-  bg.setPosition(50, 50);
-  let gameWidth = this.sys.game.config.width;
-  let gameHeight = this.sys.game.config.height;
-  console.log(gameWidth, gameHeight);
-  console.log(bg);
-  console.log(this);
-
-  this.anims.create({
-    key: "left",
-    framesL this.anims.generateFrameNumbers('warrior', {start: 0, end: 0}),
-    repeat: -1
-  });
-  this.anims.create({
-    key: "right",
-    framesL this.anims.generateFrameNumbers('warrior', {start: 2, end: 2}),
-  });
-  this.anims.create({
-    key: "up",
-    framesL this.anims.generateFrameNumbers('warrior', {start: 3, end: 3}),
-  });
-  this.anims.create({
-    key: "down",
-    framesL this.anims.generateFrameNumbers('warrior', {start: 1, end: 1}),
-    repeat: -1
-  });
-
-  player.setCollideWorldBounds(true);
-  cursors = this.input.keyboard.createCursorKeys();
-}
-
-gameScene.update = function() {
-
-}
-
-let config = {
-  type: Phaser.AUTO,
-  width: 100,
-  height: 100,
-  scene: gameScene,
-  //pixelArt: true,
-
-  parent: "game-container",
-  scene: {
-    preload: preload,
-    create: create,
-    update: update
-  }
-
+//CONFIG
+var config = {
+    type: Phaser.AUTO,
+    parent: 'content',
+    width: 320,
+    height: 240,
+    zoom: 2,
+    pixelArt: true,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: true // set to true to view zones
+        }
+    },
+    scene: [
+        BootScene,
+        SceneOne
+    ]
 };
-let game = new Phaser.Game(config);
+
+var game = new Phaser.Game(config);
+
+//PRELOAD
+function preload ()
+{
+  // spritesheet of player
+  //this.load.spritesheet('warrior', '../final-project/assets/Warrior/SpriteSheet/Warrior_Sheet-Effect.png');
+  this.load.spritesheet('player', 'assets/HobbitSpriteSheetPublished.png', { frameWidth: 60, frameHeight: 60 , margin: 0, spacing: 0 });
+
+  //full background from Tiled
+  //this.load.tilemapTiledJSON('bkg', '../final-project/assets/full-bkg.json');
+
+  //map tiles
+  //this.load.image('tiles', '../final-project/assets/tiles');
+
+  //background screenshot
+  this.load.image('background', 'assets/townscreenshot.png');
+}
+
+//CREATE
+function create ()
+{
+  this.scene.start('SceneOne');
+
+}
+
+//UPDATE
+function update ()
+{
+}
+
+
+//SCENE ONE
+var SceneOne = new Phaser.Class({
+    Extends: Phaser.Scene,
+    initialize:
+    function SceneOne ()
+    {
+        Phaser.Scene.call(this, { key: 'SceneOne' });
+    },
+
+    create: function ()
+    {
+        //var map = this.make.tilemap({ key: 'bkg' });
+        //var tiles = map.addTilesetImage('spritesheet', 'tiles');
+        // creating the layers
+        //var grass = map.createStaticLayer('Grass', tiles, 0, 0);
+        //var obstacles = map.createStaticLayer('Obstacles', tiles, 0, 0);
+        //obstacles.setCollisionByExclusion([-1]);
+        this.add.image(400, 300, 'background');
+
+//PLAYER KEY MOVEMENT
+this.anims.create({
+  key: "left",
+  framesL this.anims.generateFrameNumbers('player', {start: 0, end: 0}),
+  //frames: this.anims.generateFrameNumbers('player', { frames: [63, 64, 65, 66]}),
+  //frameRate: 10,
+  repeat: -1
+});
+this.anims.create({
+  key: "right",
+  framesL this.anims.generateFrameNumbers('player', {start: 2, end: 2}),
+});
+this.anims.create({
+  key: "up",
+  framesL this.anims.generateFrameNumbers('player', {start: 3, end: 3}),
+});
+this.anims.create({
+  key: "down",
+  framesL this.anims.generateFrameNumbers('player', {start: 1, end: 1}),
+  repeat: -1
+});
+
+//PLAYER RULES
+this.player = this.physics.add.sprite(50, 100, 'player', 6);
+//?What method is this supposed to be in?
+player.setCollideWorldBounds(true);
+cursors = this.input.keyboard.createCursorKeys();
+this.physics.world.bounds.width = map.widthInPixels;
+this.physics.world.bounds.height = map.heightInPixels;
+//haven't added obstacles yet:
+//this.physics.add.collider(this.player, obstacles);
+
+//CAMERA RULES
+this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+this.cameras.main.startFollow(this.player);
+this.cameras.main.roundPixels = true;
+
+}
